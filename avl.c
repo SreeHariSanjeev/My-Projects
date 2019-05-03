421d02733f90ee9e33e7274daddf6c7c8cb15887
SreeHariSanjeev <39899888+SreeHariSanjeev@users.noreply.github.com>
2019-05-03 23:29:47 +0530

    With one rotation
    AVL tree with only left rotation

--- avl.c
+++ avl.c
@@ -5,9 +5,10 @@ struct avl
     struct avl* right;
     int data,bal,height;
 };
+struct avl* ROOT = NULL;
 int main()
 {
-    struct avl *root = NULL,*parent = NULL;
+    struct avl *parent = NULL,*parent1 = NULL;
 
     int ch,data;
     printf("1.Insert\n2.Delete\n3.Traverse\n4.Exit");
@@ -19,19 +20,19 @@ int main()
         {
             case 1: printf("Enter the number: ");
                     scanf("%d",&data);
-                    Insert(&root,data);
+                    Insert(&ROOT,data,&parent1);
                     break;
             case 2: printf("\nEnter the number: ");
                     scanf("%d",&data);
-                    Delete(&root,data,&parent);
+                    Delete(&ROOT,data,&parent);
                     break;
-            case 3: Traverse(root);
+            case 3: Traverse(ROOT);
 
         }
 
     }
 }
-void Insert(struct avl** root ,int data)
+void Insert(struct avl** root ,int data,struct avl** parent)
 {
     int h;
     struct avl* temp = (struct avl*)malloc(sizeof(struct avl));
@@ -47,41 +48,18 @@ void Insert(struct avl** root ,int data)
         return;
     }
     struct avl* rt = *root;
+    struct avl* par = *parent;
     if(rt->data >= data)
-         Insert(&(rt->left),data);
+             Insert(&(rt->left),data,&rt);
+
     else
-         Insert(&(rt->right),data);
+             Insert(&(rt->right),data,&rt);
 
+          CalculateBal(&rt);
+          Balance(&rt,&par);
+             /*if(par != NULL)
+          printf("par %d rt: %d ",par->data,rt->data);*/
 
-      if(rt->right == NULL || rt->left ==NULL)
-            {
-                if(rt->right != NULL)
-                   {
-                     rt->height = rt->right->height + 1;
-                     h = rt->right->height;
-                     rt->bal = -h-1;
-                   }
-                else
-                    {
-                         rt->height= rt->left->height + 1;
-                         rt->bal = rt->left->height +1;
-                    }
-                //rt->height++;
-            }
-        else
-        {
-            if(rt->left->height> rt->right->height)
-                {
-                    h = rt->left->height;
-                    rt->height = ++h;
-                }
-                else
-                {
-                    h = rt->right->height;
-                    rt->height = ++h;
-                }
-        rt->bal = rt->left->height - rt->right->height;
-        }
 
 }
 void Traverse(struct avl* root)
@@ -105,10 +83,10 @@ void Delete(struct avl** root,int data,struct avl** par)
     if(rt->data<data)
             if(rt->right != NULL)
                {
-                 //parent = *root;
+                 parent = *root;
                  Delete(&(rt->right),data,&parent);
                  printf("\n parent: %d",rt->data);
-                 printf("\nRight");
+                 printf(" Right");
                }
             else                        //item not found
                 printf("Item not found");
@@ -117,16 +95,17 @@ void Delete(struct avl** root,int data,struct avl** par)
 
                if(rt->left != NULL)
                    {
-                     //parent = *root;
+                     parent = *root;
                      Delete(&(rt->left),data,&parent);
                      printf("\n parent: %d",rt->data);
-                     printf("\nLeft");
+                     printf(" Left");
                    }
                 else
                     printf("Item not found");
            }
-    else
-    {
+
+     if(rt->data == data){
+            printf("%d found",rt->data);
         if(parent == NULL)             //If root element is deleted we can't traverse the tree
         {
             free(rt);
@@ -136,7 +115,7 @@ void Delete(struct avl** root,int data,struct avl** par)
 
         if(rt->left != NULL && rt->right != NULL)
         {
-            printf("\ntwo child");
+
             parent = rt;
             xsucc = rt->right;
             while(xsucc->left != NULL)
@@ -148,10 +127,11 @@ void Delete(struct avl** root,int data,struct avl** par)
             printf("sc:%d",xsucc->data);
             rt = xsucc;                 //node with either 1 or 0 child (inoder succusor)
 
+            printf("two child");
         }
         if(rt->left == NULL && rt->right ==NULL)
         {
-            printf("\nno child");
+
             if(parent != NULL)              //parent is NULL if root element
             {
                 printf("Paren %d",parent->data);
@@ -164,12 +144,13 @@ void Delete(struct avl** root,int data,struct avl** par)
 
             free(rt);
             rt = NULL;
+             printf("no child");
             return;
         }
 
         if(rt->left != NULL&& parent != NULL)
         {
-            printf("\nleft child");
+
                 if(parent->left == rt)
                     parent->left = rt->left;
                 else
@@ -177,11 +158,12 @@ void Delete(struct avl** root,int data,struct avl** par)
 
             free(rt);
             rt = NULL;
+            printf("left child");
             return;
         }
         if(rt->right != NULL&&parent != NULL)
         {
-            printf("\nRight child");
+            printf("Right child");
             if(parent->left == rt)
                 parent->left = rt->right;
             else
@@ -192,10 +174,18 @@ void Delete(struct avl** root,int data,struct avl** par)
             return;
 
         }
+        return;
 
     }
+    printf(" balancing");
+    if(rt->left == NULL && rt->right == NULL)
+    {
+        rt->height = 0;
+        rt->bal = 0;
+        return;
+    }
 
-    if(rt->right == NULL || rt->left ==NULL)
+    if(rt->right == NULL || rt->left ==NULL)                //only one child
         {
             if(rt->right != NULL)
                    {
@@ -205,11 +195,11 @@ void Delete(struct avl** root,int data,struct avl** par)
                    }
                 else                                        //Left child present
                     {
-                        rt->left = rt->left->height + 1;
+                        rt->height = rt->left->height + 1;
                          rt->bal = rt->left->height +1;
                     }
         }
-        else
+        else                                                //two child
         {
             if(rt->left->height> rt->right->height)
                 {
@@ -223,46 +213,129 @@ void Delete(struct avl** root,int data,struct avl** par)
                 }
         rt->bal = rt->left->height - rt->right->height;
         }
+        Balance(&rt);
+
+    return;
+
+
+}
+void CalculateBal(struct avl** root)
+{
+    int h;
+    struct avl* rt = *root;
     if(rt->left == NULL && rt->right == NULL)
     {
         rt->height = 0;
         rt->bal = 0;
-    }
-    return;
-
-    /*if(found == 0)
-    {
-        printf("Item not found");
         return;
-    }*/
+    }
+
+    if(rt->right == NULL || rt->left ==NULL)                //only one child
+        {
+            if(rt->right != NULL)
+                   {
+                     rt->height = rt->right->height + 1;
+                     h = rt->right->height;
+                     rt->bal = -h-1;                        //negative value
+                   }
+                else                                        //Left child present
+                    {
+                        rt->height = rt->left->height + 1;
+                         rt->bal = rt->left->height +1;
+                    }
+        }
+        else                                                //two child
+        {
+            if(rt->left->height> rt->right->height)
+                {
+                    h = rt->left->height;
+                    rt->height = ++h;
+                }
+                else
+                {
+                    h = rt->right->height;
+                    rt->height = ++h;
+                }
+        rt->bal = rt->left->height - rt->right->height;
+        }
+}
+void Balance(struct avl** node,struct avl** parent)
+{
+    struct avl* par = *parent;
+    struct avl* temp = *node;
+    struct avl* pivot;
+    int bal = temp->bal;
+    if(bal == -2)
+    {
+        pivot = temp->right;
+        if(pivot->bal<0)
+           {
+             printf("Left rotation : %d",temp->data);
+             LeftRotate(&temp,&par);
+           }
+        else
+           {
+             printf("Right Left rotation %d",temp->data);
+           }
+    }
+    if(bal == 2)
+    {
+        pivot = temp->left;
+        if(pivot->bal>0)
+           {
+             printf("Right rotation : %d",temp->data);
+             RightRotate(&temp);
+           }
+        else
+            {
+                printf("Left right rotation %d",temp->data);
+            }
+    }
+}
+void LeftRotate(struct avl** root,struct avl** parent)
+{
+    struct avl* par = *parent;
+    struct avl* rt = *root;
+    //struct avl* RT = *root;
+
+    struct avl* pivot = rt->right;
+    struct avl* Y = pivot->left;
+
+    rt->right = NULL;           //disconnect
+    pivot->left = rt;
+
+    if(Y != NULL)
+        rt->right = Y;
+
+    if(par!=NULL)
+    {
+      par->right = pivot;                   //connect pivot with parent
+      printf(" parn %d",par->data);
+    }
+    else                                //left rotation of root element
+    {
+        ROOT = pivot;                  //make pivot as root
+    }
+
+    //rt = pivot;
+    printf("\npivot left %d pivot right: %d ",pivot->left->data,pivot->right->data);
+
+    CalculateBal(&rt);                          //first child node bal is calculated
+    CalculateBal(&pivot);
+
+    printf("\n pivbal: %d rtbal: %d",pivot->bal,rt->bal);
 
 }
-void search(struct avl* root,struct avl**rt,int data,struct avl** parent,int* found)
+void RightRotate(struct avl** root)
 {
-    *found = 0;
-    *parent = NULL;
-    while(1)
-    {
 
-        if(root->data == data)
-        {
-            *found = 1;
-             break;
-        }
-        *parent = root;
+}
+void LeftRight(struct avl** root)
+{
 
-        if(root->data<data)
-            if(root->right != NULL)
-                root = root->right;
-            else                        //item not found
-                break;
-        else
-            if(root->left != NULL)
-                root = root->left;
-            else
-                break;
-    }
-    *rt = root;
+}
+void RightLeft(struct avl** root)
+{
 
 }
 
